1de8ab7dc6ffda0ec7dbb4484909b71339920db5
cheolwon2889 <114125050+cheolwon2889@users.noreply.github.com>
2024-12-21 15:51:38 +0900

    요청

--- website/src/main/java/com/example/demo/service/OpenURLService.java
+++ website/src/main/java/com/example/demo/service/OpenURLService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OpenURLService {
-
+	String ServiceKey = "";
+	String ObsCode = "DT_0001";
+//	
 	public void getWeather() {
 		
 	}
