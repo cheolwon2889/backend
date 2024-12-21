4ea1e303dc2661e4e91877c568008f5ee0c65783
cheolwon2889 <114125050+cheolwon2889@users.noreply.github.com>
2024-12-21 12:53:59 +0900

    RestController, add OpenURLService, add WebConfig 리액트 연동완료

--- website/src/main/java/com/example/demo/controller/TestController.java
+++ website/src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,38 @@
 package com.example.demo.controller;
 
+import java.io.Console;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.OpenURLService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TestController {
+	
+	@Autowired
+	private OpenURLService openUService;
+	
+	@GetMapping("/")
+	public void main() {
+		System.out.println("main 실행");
+	}
+	
+	@GetMapping("/hello")
+	public void hello() {
+		System.out.println("hello 실행");
+	}
+	
+	@GetMapping("/weather")
+	public void weatherGET() {
+		openUService.getWeather();
+	}
+	
+	
 
 }
