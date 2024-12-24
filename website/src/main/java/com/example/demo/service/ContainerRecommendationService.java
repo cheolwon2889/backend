4339d31e846f32f2a37c242b346265bdb7f16590
cheolwon2889 <114125050+cheolwon2889@users.noreply.github.com>
2024-12-24 15:10:19 +0900

    커밋

--- website/src/main/java/com/example/demo/service/ContainerRecommendationService.java
+++ website/src/main/java/com/example/demo/service/ContainerRecommendationService.java
@@ -20,11 +20,22 @@ public class ContainerRecommendationService {
         List<String> routeList = Arrays.asList(route.split(","));
 		if(routeList != null) {
 			// 컨테이너 List 받아오기
+			// dao.getContainerList();
 			
+			for(int i = 0; i < ContainerList.size();i++) {
+				System.out.println(ContainerList.get(i).getPort());
+				
+			}
+			for(ContainerVO list : ContainerList) {
+				System.out.println(list.getPort());
+			}
 		}
 		List<String> ports = new ArrayList<>();
 		
 		//   하역 항구끼리 List 만듬.
 		
+		// 층 별로 알고리즘화 해서 알려주기
+		
+		// 시뮬레이션을 돌리며서. 쌓아보면서. 구현
 	}
 }
